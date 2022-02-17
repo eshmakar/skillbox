468088d567ab17371a051e62157e8ef3674a8e63
eshmakar <59125020+eshmakar@users.noreply.github.com>
2022-02-17 23:00:28 +0300

    Initial commit

---
+++ src/com/test/DateTest.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateTest {
+    public static void main(String[] args) {
+        DateFormat americanFormat = new SimpleDateFormat("hh:mm:ss a MM.dd.yyyy");
+        Date date = new Date();
+        System.out.println(americanFormat.format(date)); //10:57:03 PM 02.17.2022
+
+        DateFormat russianFormat = new SimpleDateFormat("HH:mm:ss dd.MM.yyyy");
+        System.out.println(russianFormat.format(date)); //22:59:37 17.02.2022
+
+
+    }
+}
